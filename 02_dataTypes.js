eb61d83e645bee9c50225de6fe850d2bf4dc0617
Abhijeet Kumar <abhijeetkumar3245@gmail.com>
2024-10-17 16:45:06 +0530

    Data_Types

---
+++ 02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version;
+
+// alert(3+3)-> we are using nodejs, not browser
+
+//PRIMITIVE DATA TYPES
+//number => 2 to the power 56
+//bigint
+//string=>""
+//boolean=>true/false;
+//null=> standalone value (khali hai)
+//undefined => value not assigned
+//symbol
+
+//NON_PRIMITIVE DATA TYPES
+//object
+//Array
+//FUnctions
+
+console.log(typeof(undefined)); //undefined
+console.log(typeof(null));// null is object in js
+
