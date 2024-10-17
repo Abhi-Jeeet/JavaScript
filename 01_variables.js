9037e0328a3396931a54439038a18ea11b0f3498
Abhijeet Kumar <abhijeetkumar3245@gmail.com>
2024-10-17 16:44:48 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="abhijeet@google.com";
+var accountPassword=12345
+accountCity="Bengaluru"
+let accountState; // output-> undefined
+
+// accountId=2 ->we cant reassigned const variable
+
+accountEmail="ab@gmail.com"
+accountPassword=565456
+accountCity="jaipur"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
