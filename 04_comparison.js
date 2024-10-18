23c23ea1118591c40d17fe06737e8e225f189b32
Abhijeet Kumar <abhijeetkumar3245@gmail.com>
2024-10-18 20:56:00 +0530

    comparison

---
+++ 04_comparison.js
@@ -0,0 +1,21 @@
+ console.log("2">1);//true
+ console.log("02">1);//true
+
+ console.log(null>0);//false
+ console.log(null==0);//false
+ console.log(null>=0);//true
+ console.log(null===0);//false
+
+ console.log(undefined==0);//false
+ console.log(undefined===0);//false
+ console.log(undefined>=0);//false
+
+
+ // === ->it checks datatypes also
+ console.log("4"==4);//true
+ console.log("4"===4);//false
+ 
+ 
+ 
+ 
+ 
