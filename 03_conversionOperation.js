6d8e43bb7311d7b9949057ad2fa42a925f0f3caf
Abhijeet Kumar <abhijeetkumar3245@gmail.com>
2024-10-18 20:55:51 +0530

    conversion operations

---
+++ 03_conversionOperation.js
@@ -0,0 +1,45 @@
+let score =false
+let inNumber=Number(score);
+
+console.log(typeof inNumber)
+console.log(inNumber)
+
+/*
+converting to number
+"33" => 33
+"33abc" => NaN (Not a number)
+true=> 1
+false=> 0
+*/
+
+let isLoggedIn="a"
+let toBoolean=Boolean(isLoggedIn)
+console.log(toBoolean);
+
+/* 
+converting to boolean
+0 => false
+1 => true
+"" => false
+"Abhijeet" => true
+*/
+
+
+/*******Operations***********/
+
+let value=3
+let negative=-value
+console.log(negative); //-3
+
+let str1="hello"
+let str2=" abhijeet"
+let str3=str1+str2
+console.log(str3); //hello abhijeet
+
+console.log("1"+2); //12
+console.log(2+"1");//21
+console.log("1" +2 +2);//122
+console.log(1+2+"2");//32
+
+
+
